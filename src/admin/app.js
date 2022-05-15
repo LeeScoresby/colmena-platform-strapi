1a4b4793bd2571d3d03c577d93e9257ac8f00e4b
Borja <borjarodrilore@gmail.com>
2022-05-15 23:35:13 +0200

    Some changes to continue building the schema

--- src/admin/app.js
+++ src/admin/app.js
@@ -5,6 +5,7 @@ export default {
   config: {
     locales: [
       'es',
+      'en',
     ],
     auth: {
       logo: AuthLogo,
@@ -16,12 +17,22 @@ export default {
       logo: Favicon,
     },
     translations: {
+      en: {
+        'Auth.form.welcome.title': 'Welcome to Colmena!',
+        'Auth.form.welcome.subtitle': 'Log in to your account',
+        'app.components.LeftMenu.navbrand.title': 'Colmena Dashboard'
+      },
       es: {
         'Auth.form.welcome.title': '¡Bienvenido a Colmena!',
         'Auth.form.welcome.subtitle': 'Inicie sesión con su cuenta',
         'app.components.LeftMenu.navbrand.title': 'Colmena Dashboard'
       }
     },
+    theme: {
+      colors: {
+          primary100: '#00000',
+      }
+    }
   },
   bootstrap(app) {
     // console.log(app);
